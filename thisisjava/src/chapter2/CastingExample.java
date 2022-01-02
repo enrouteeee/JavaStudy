04d9078fb620d87cd61aed5c69494113b3d9ead4
enrouteeee <enrouteeee@gmail.com>
2022-01-02 22:09:56 +0900

    chater2.변수와 타입

---
+++ thisisjava/src/chapter2/CastingExample.java
@@ -0,0 +1,9 @@
+package chapter2;
+
+public class CastingExample {
+    public static void main(String[] args) {
+        int intValue = 103029770;
+        byte byteValue = (byte) intValue;
+        System.out.println("byteValue = " + byteValue);
+    }
+}
